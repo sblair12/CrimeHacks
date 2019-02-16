648fcdc687f7e9fdb6b37958f6537b4eb85c7347
Joel Ki <joelki197@gmail.com>
2019-02-16 03:11:26 -0600

    update files

---
+++ public/js/main.js
@@ -0,0 +1,11 @@
+
+
+var margin = {top: 40, right: 10, bottom: 60, left: 60};
+
+var width = 845 - margin.left - margin.right,
+	height = 535 - margin.top - margin.bottom;
+
+var svg = d3.select("#onCampus").append("svg")
+	.attr("width", width + margin.left + margin.right)
+	.attr("height", height + margin.top + margin.bottom)
+	.attr("id","canvas")
