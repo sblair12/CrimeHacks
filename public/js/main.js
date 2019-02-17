169aec6f4487ba6fb74d66b4155f0e93d5fa164c
Shane Blair <sblair@wustl.edu>
2019-02-17 12:31:38 -0600

    final

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,7 @@ d3.queue()
 	.defer(d3.json, "/data")
 	.await(getData);
 let globalData;
+let globalBounds;
 let status = true;
 let holder = 0;
 var margin = {top: 40, right: 10, bottom: 60, left: 10};
@@ -24,90 +25,268 @@ let crimeStrings = ["burglary", "theft", "assault", "carjacking", "sexual", "non
 //let crimeColors = ["black", "orange", "red", "yellow", ""]
 var colorScale = d3.scaleOrdinal(d3.schemeCategory10)
 	.domain(crimeStrings);
+let view = "locations";
+
+let coords = [
+	{
+		x: 100,
+		y: 375,
+		width: 320,
+		height: 330
+	},
+	{
+		x: 110,
+		y: 103,
+		width: 210,
+		height: 255
+	},
+	{
+		x: 116,
+		y: 27,
+		width: 230,
+		height: 75
+	},
+	{
+		x: 320,
+		y: 245,
+		width: 90,
+		height: 115
+	},
+	{
+		x: 345,
+		y: 175,
+		width: 145,
+		height: 65
+	},
+	{
+		x: 347,
+		y: 108,
+		width: 290,
+		height: 65
+	},
+	{
+		x: 415,
+		y: 245,
+		width: 55,
+		height: 115
+	},
+	{
+		x: 505,
+		y: 175,
+		width: 52,
+		height: 80
+	},  {
+		x: 475,
+		y: 258,
+		width: 76,
+		height: 100
+	},
+	{
+		x: 580,
+		y: 175,
+		width: 59,
+		height: 94
+	},
+	{
+		x: 575,
+		y: 275,
+		width: 150,
+		height: 85
+	},
+	{
+		x: 645,
+		y: 110,
+		width: 80,
+		height: 160
+	},
+	{
+		x: 730,
+		y: 200,
+		width: 60,
+		height: 60
+	},
+	{
+		x: 730,
+		y: 270,
+		width: 120,
+		height: 90
+	},
+	{
+		x: 730,
+		y: 270,
+		width: 120,
+		height: 90
+	},
+	{
+		x: 740,
+		y: 105,
+		width: 300,
+		height: 69
+	},
+	{
+		x: 820,
+		y: 200,
+		width: 60,
+		height: 57
+	},
+	{
+		x: 881,
+		y: 260,
+		width: 175,
+		height: 100
+	},
+	{
+		x: 425,
+		y: 375,
+		width: 190,
+		height: 70
+	},
+];
 
 function getData (error, data) {
-	globalData = data;
-	update(data);
+	globalData = data.crimes.filter((elem) => elem.category !== "");
+	globalBounds = data.bounds;
+	update(globalData);
 }
 
 function update(data) {
-	console.log(data);
-	spaceItems(data);
+    if (view === "regions") {
+        svg.selectAll("circle")
+            .transition()
+            .duration(1000)
+            .attr("opacity", 0);
+        svg2.selectAll("circle")
+            .transition()
+            .duration(1000)
+            .attr("opacity", 0);
 
-	var xScale2 = d3.scaleLinear()
-		.domain([-90.316390,-90.300687])
-		.range([0,width2]);
-	var yScale2 = d3.scaleLinear()
-		.domain([38.64291, 38.651754])
-		.range([height2,0]);
-	var svgSaver2 = svg2.selectAll("circle")
-		.data(data);
-	svgSaver2.enter()
-		.append("circle")
-		.merge(svgSaver2)
-		.attr("cx",function (d) {
-			if (d.gotLocation) {
-				return xScale2(d.lon);
-			}
-			else return -1;
-		})
-		.attr("cy",function(d){
-			if (d.gotLocation) return yScale2(d.lat);
-			else return -1;
-		})
-		.attr("r", 4)
-		.attr("fill",function(d){
-			return colorScale(d.category);
-		})
-		.on("click",function(d){
-			console.log(d.lat + ", " + d.lon)
-		});
-	svgSaver2.exit().transition().delay(500).duration(500).remove();
+        var oranges = ["#fff5eb","#fee7cf","#fdd4ab","#fdb97e","#fd9c51","#f77d2a","#e85e0e","#cc4503","#a33503","#7f2704"];
+        var heatScale = d3.scaleQuantize()
+			.domain([
+				d3.min(globalBounds, d => d.number),
+				d3.max(globalBounds, d => d.number)
+			])
+			.range(oranges);
 
-	svg2.transition().delay(1500).duration(1000).attr("opacity", 1);
-
-	var xScale = d3.scaleLinear()
-		.domain([-90.328869, -90.274651])
-		.range([0, width]);
-	var yScale = d3.scaleLinear()
-		.domain([38.642568,38.661009])
-		.range([height, 0]);
-
-	var svgSaver = svg.selectAll("circle")
-		.data(data);
-		svgSaver.enter()
-		.append("circle")
-			.merge(svgSaver)
-
-			.attr("cx",function (d) {
-				if (d.gotLocation) return xScale(d.lon);
-				else return -1;
+		var heats = svg2.selectAll("rect")
+			.data(globalBounds);
+		heats
+			.enter()
+			.append("rect")
+			.merge(heats)
+			.attr("x", (d, index) => coords[index].x)
+			.attr("y", (d, index) => coords[index].y)
+			.attr("width", (d, index) => coords[index].width)
+			.attr("height", (d, index) => coords[index].height)
+			.attr("transform", "rotate(7)")
+			.attr("opacity", 0.6)
+			.attr("fill", d => heatScale(d.number))
+			.on("mouseover", function() {
+				d3.select(this).attr("opacity", 0.8);
 			})
-			.attr("cy",function(d){
-				if (d.gotLocation) return yScale(d.lat);
-				else return -1;
-			})
-			.attr("r",4)
-			.attr("fill",function(d){
-				return colorScale(d.category);
-			})
-			.on("click",function(d){
-				console.log(d.lat + ", " + d.lon)
+			.on("mouseout", function() {
+				d3.select(this).attr("opacity", 0.6);
 			});
 
-		svgSaver.exit().transition().delay(500).duration(500).remove();
+		var tip = d3.tip().attr('class', 'd3-tip').html(function(d) {
+			return `<div id="tip-title" class="tip-title">${d.name}</div><div id="tip-desc" class="tip-desc">${d.number}</div>`;
+		});
+		tip.direction('n');
+		tip.offset([-5, 0]);
 
-	var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return `<div class="tip-title">${d.location}</div><div class="tip-desc">${(d.category === undefined) ? "" : d.category.charAt(0).toUpperCase() + d.category.slice(1)}</div>`; });
-	tip.direction('n');
-	tip.offset([-5, 0]);
+		svg.call(tip);
+		svg.selectAll("rect").on("mouseover", tip.show)
+			.on("mouseout", tip.hide);
 
-	svg.call(tip);
-	svg.selectAll("circle").on("mouseover", tip.show)
-		.on("mouseout", tip.hide);
+		svg2.call(tip);
+		svg2.selectAll("rect").on("mouseover", tip.show)
+			.on("mouseout", tip.hide);
 
-	svg2.call(tip);
-	svg2.selectAll("circle").on("mouseover", tip.show)
-		.on("mouseout", tip.hide);
+        svg2.transition().delay(1500).duration(1000).attr("opacity", 1);
+        svg.transition().delay(1500).duration(1000).attr("opacity", 1);
+    }
+    else {
+		svg.selectAll("rect")
+			.transition()
+			.duration(1000)
+			.attr("opacity", 0);
+		svg2.selectAll("rect")
+			.transition()
+			.duration(1000)
+			.attr("opacity", 0);
+
+        var xScale2 = d3.scaleLinear()
+            .domain([-90.316390,-90.300687])
+            .range([0,width2]);
+        var yScale2 = d3.scaleLinear()
+            .domain([38.64291, 38.651754])
+            .range([height2,0]);
+        var svgSaver2 = svg2.selectAll("circle")
+            .data(data);
+        svgSaver2.enter()
+            .append("circle")
+            .merge(svgSaver2)
+            .attr("cx",function (d) {
+                if (d.gotLocation) {
+                    return xScale2(d.lon);
+                }
+                else return -1;
+            })
+            .attr("cy",function(d){
+                if (d.gotLocation) return yScale2(d.lat);
+                else return -1;
+            })
+            .attr("r", 4)
+            .attr("fill",function(d){
+                return colorScale(d.category);
+            })
+            .attr("opacity", 1)
+        svgSaver2.exit().transition().delay(500).duration(500).remove();
+
+        svg2.transition().delay(1500).duration(1000).attr("opacity", 1);
+
+        var xScale = d3.scaleLinear()
+            .domain([-90.328869, -90.274651])
+            .range([0, width]);
+        var yScale = d3.scaleLinear()
+            .domain([38.642568,38.661009])
+            .range([height, 0]);
+
+        var svgSaver = svg.selectAll("circle")
+            .data(data);
+        svgSaver.enter()
+            .append("circle")
+            .merge(svgSaver)
+
+            .attr("cx",function (d) {
+                if (d.gotLocation) return xScale(d.lon);
+                else return -1;
+            })
+            .attr("cy",function(d){
+                if (d.gotLocation) return yScale(d.lat);
+                else return -1;
+            })
+            .attr("r",4)
+            .attr("fill",function(d){
+                return colorScale(d.category);
+            })
+            .attr("opacity", 1)
+
+        svgSaver.exit().transition().delay(500).duration(500).remove();
+        svg.transition().delay(1500).duration(1000).attr("opacity", 1);
+
+        var tip = d3.tip().attr('class', 'd3-tip').html(function(d) { return `<div class="tip-title">${d.location}</div><div class="tip-desc">${(d.category === undefined) ? "" : d.category.charAt(0).toUpperCase() + d.category.slice(1)}</div>`; });
+        tip.direction('n');
+        tip.offset([-5, 0]);
+
+        svg.call(tip);
+        svg.selectAll("circle").on("mouseover", tip.show)
+            .on("mouseout", tip.hide);
+
+        svg2.call(tip);
+        svg2.selectAll("circle").on("mouseover", tip.show)
+            .on("mouseout", tip.hide);
+    }
 
 	if (status) {
 		svg.attr("opacity", 0.15);
@@ -119,58 +298,13 @@ function update(data) {
 	d3.select("#ranking-type").on("change", filterMatch);
 }
 
-function spaceItems(data) {
-	var set = new Set();
-
-	var offset = 0.000025;
-	//var alternate = 0;
-	data.forEach((elem, index) => {
-		if (elem.gotLocation) {
-			if (set.has(elem.lon*elem.lat)) {
-				while(true) {
-					//console.log(index);
-					var random = Math.floor(Math.random() * 8);
-					//console.log(random);
-					switch (random) {
-						case 0:
-							elem.lon += offset;
-							break;
-						case 1:
-							elem.lat += offset;
-							break;
-						case 2:
-							elem.lon -= offset;
-							break;
-						case 3:
-							elem.lat -= offset;
-							break;
-						case 4:
-							elem.lon += offset;
-							elem.lat += offset;
-							break;
-						case 5:
-							elem.lon -= offset;
-							elem.lat -= offset;
-							break;
-						case 6:
-							elem.lon += offset;
-							elem.lat -= offset;
-							break;
-						case 7:
-							elem.lon -= offset;
-							elem.lat += offset;
-							break;
-					}
-					if (!set.has(elem.lon*elem.lat)) break;
-				}
-				//alternate = (alternate + 1) % 8;
-				set.add(elem.lon*elem.lat);
-			}
-			else {
-				set.add(elem.lon*elem.lat);
-			}
-		}
-	});
+function changeView() {
+    svg2.transition().duration(1000).attr("opacity", 0.15);
+    svg.transition().duration(1000).attr("opacity", 0.15);
+    view = d3.select('input[name="options"]:checked').node().value;
+    d3.select('#ranking-type').property('value', 'all');
+    d3.select('#keywordSearch').property('value', "");
+    update(globalData);
 }
 
 function filterMatch(e) {
@@ -182,7 +316,8 @@ function filterMatch(e) {
 	}
 	else {
 		var filtered = (keyword === "") ? globalData : globalData.filter((elem) => elem.description.toLowerCase().includes(keyword.toLowerCase()));
-        filtered = (filter === "all") ? filtered : filtered.filter((elem) => elem.type.toLowerCase().includes(filter.toLowerCase()) || elem.description.toLowerCase().includes(filter.toLowerCase()));
+		if (filter === "other") filtered = filtered.filter((elem) => elem.category === "");
+		else filtered = (filter === "all") ? filtered : filtered.filter((elem) => elem.type.toLowerCase().includes(filter.toLowerCase()) || elem.description.toLowerCase().includes(filter.toLowerCase()));
 		update(filtered);
 	}
     // var options = {
--- server.js
+++ server.js
@@ -24,6 +24,7 @@ var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const Crime = require('./models/crime.js');
+const boundsJson = require('./bounds.json');
 
 //ENDPOINTS
 app.get('/data', (req, res) => {
@@ -33,13 +34,113 @@ app.get('/data', (req, res) => {
       if (err) {
         res.send(err);
       }
-	  res.json(crimes);
+
+      let bounds = spaceItems(crimes);
+
+	  res.send({
+          success: true,
+          crimes: crimes,
+          bounds: bounds
+	  });
     });
 
 });
 
+let crimeStrings = ["burglary", "theft", "assault", "carjacking", "sexual", "nonviolent"];
+let crimeBack = {
+  burglary: 0,
+  theft: 1,
+  assault: 2,
+  carjacking: 3,
+  sexual: 4,
+  nonviolent: 5
+};
+
+function spaceItems(data) {
+    var set = new Set();
+
+    let bounds = [];
+    boundsJson.forEach((bound, index) => {
+        bounds.push({
+            name: bound.name,
+            number: 0,
+            types: []
+        });
+        crimeStrings.forEach((thing) => {
+            bounds[index].types.push({
+                name: thing,
+                value: 0
+            });
+        })
+        // bounds.list = [];
+        // bounds[index].push(bound.name);
+    });
+    //console.log(bounds);
+    var offset = 0.000025;
+    //var alternate = 0;
+    data.forEach((elem, index) => {
+        if (elem.gotLocation && elem.lon !== undefined && elem.lat !== undefined) {
+            if (set.has(elem.lon*elem.lat)) {
+                while(true) {
+                    //console.log(elem.lon);
+                    var random = Math.floor(Math.random() * 8);
+                    //console.log(random);
+                    switch (random) {
+                        case 0:
+                            elem.lon += offset;
+                            break;
+                        case 1:
+                            elem.lat += offset;
+                            break;
+                        case 2:
+                            elem.lon -= offset;
+                            break;
+                        case 3:
+                            elem.lat -= offset;
+                            break;
+                        case 4:
+                            elem.lon += offset;
+                            elem.lat += offset;
+                            break;
+                        case 5:
+                            elem.lon -= offset;
+                            elem.lat -= offset;
+                            break;
+                        case 6:
+                            elem.lon += offset;
+                            elem.lat -= offset;
+                            break;
+                        case 7:
+                            elem.lon -= offset;
+                            elem.lat += offset;
+                            break;
+                    }
+                    if (!set.has(elem.lon*elem.lat)) break;
+                }
+                //alternate = (alternate + 1) % 8;
+                set.add(elem.lon*elem.lat);
+            }
+            else {
+                set.add(elem.lon*elem.lat);
+            }
+            //console.log(boundsJson);
+            for (var x = 0; x < boundsJson.length; x++) {
+                //console.log(`lat: ${elem.lat} -> ${boundsJson[x].topLat} ${boundsJson[x].bottomLat}`);
+                //console.log(`lon: ${elem.lon} -> ${boundsJson[x].topLon} ${boundsJson[x].bottomLon}`);
+                if (elem.lat <= boundsJson[x].topLat && elem.lat >= boundsJson[x].bottomLat &&
+                    elem.lon >= boundsJson[x].topLon && elem.lon <= boundsJson[x].bottomLon) {
+                    //console.log("push");
+                    bounds[x].number += 1;
+                    if (elem.category !== "") bounds[x].types[crimeBack[elem.category]].value += 1;
+                    break;
+                }
+            }
+        }
+    });
+    return bounds;
+}
+
 app.post('/submitForm', (req, res) => {
-  console.log("body: " + JSON.stringify(req.body));
   var crimeData = {
     date: req.body.date,
     time: req.body.time,
@@ -50,7 +151,6 @@ app.post('/submitForm', (req, res) => {
     location: req.body.location,
     gotLocation: true
   };
-  console.log(crimeData);
   Crime.create(crimeData, function(error, crime) {
     if (error) {
         console.log(error);
